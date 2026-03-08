8372c3f69df6d520fbca44608f46549de1c685ae
teiwiet <teiwiet33@gmail.com>
2026-03-08 22:07:58 +0700

    update nvim

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,5 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+vim.keymap.set("n", "<A-l>", "<cmd>BufferLineMoveNext<cr>", { desc = "Move buffer left" })
+vim.keymap.set("n", "<A-h>", "<cmd>BufferLineMovePrev<cr>", { desc = "Move buffer left" })
---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,8 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.tabstop = 4 -- 1 tab = 4 spaces
+vim.opt.shiftwidth = 4 -- indent = 4 spaces
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true -- dùng space thay vì tab thật
+vim.g.autoformat = false
